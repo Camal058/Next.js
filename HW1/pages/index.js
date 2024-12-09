f1c957c29dfc3b16db4f3a0af2b7d5a81c6e1b9d
Camal <87723982+Camal058@users.noreply.github.com>
2024-12-09 21:16:39 +0400

    updated with ssg and ssr

--- HW1/pages/index.js
+++ HW1/pages/index.js
@@ -1,16 +1,23 @@
 import Link from 'next/link';
-import { categories } from '../data/data';
 
-export default function HomePage() {
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3001/categories');
+  const categories = await res.json();
+
+  return {
+    props: { categories },
+  };
+}
+
+
+export default function Home({ categories }) {
   return (
     <div>
       <h1>Categories</h1>
       <ul>
         {categories.map((category) => (
           <li key={category.id}>
-            <Link href={`/category/${category.id}`}>
-              {category.name}
-            </Link>
+            <Link href={`/category/${category.id}`}>{category.name}</Link>
           </li>
         ))}
       </ul>
