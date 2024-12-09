7423289d64c2a264d67d8f8670db5c5c8e5952b2
Camal <87723982+Camal058@users.noreply.github.com>
2024-12-09 21:17:27 +0400

    u

--- HW1/pages/category/[categoryId].js
+++ HW1/pages/category/[categoryId].js
@@ -1,30 +1,40 @@
-import { useRouter } from 'next/router';
 import Link from 'next/link';
-import { categories } from '../../data/data';
 
-export default function CategoryPage() {
-  const router = useRouter();
-  const { categoryId } = router.query;
+export async function getStaticPaths() {
+  const res = await fetch('http://localhost:3001/categories');
+  const categories = await res.json();
 
-  const category = categories.find((cat) => cat.id === categoryId);
+  const paths = categories.map((category) => ({
+    params: { categoryId: category.id.toString() },
+  }));
 
-  if (!category) {
-    return <p>Category not found</p>;
-  }
+  return { paths, fallback: false };
+}
 
+export async function getStaticProps({ params }) {
+  const categoryRes = await fetch(`http://localhost:3001/categories/${params.categoryId}`);
+  const category = await categoryRes.json();
+
+  const postsRes = await fetch(`http://localhost:3001/posts?categoryId=${params.categoryId}`);
+  const posts = await postsRes.json();
+
+  return {
+    props: { category, posts },
+  };
+}
+
+
+export default function Category({ category, posts }) {
   return (
     <div>
-      <h1>{category.name}</h1>
+      <h1>Category: {category.name}</h1>
       <ul>
-        {category.posts.map((post) => (
+        {posts.map((post) => (
           <li key={post.id}>
-            <Link href={`/category/${categoryId}/post/${post.id}`}>
-              {post.title}
-            </Link>
+            <Link href={`/category/${category.id}/post/${post.id}`}>{post.title}</Link>
           </li>
         ))}
       </ul>
-      <Link href="/">Back to Categories</Link>
     </div>
   );
 }
